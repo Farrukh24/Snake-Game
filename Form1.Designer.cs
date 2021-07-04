1965a3c63faa50e132e84b3db3687f396dbae8fa
Farrukh <72334165+Farrukh24@users.noreply.github.com>
2021-07-04 20:24:26 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelFood = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.ForeColor = System.Drawing.Color.Orchid;
+            this.labelScore.Location = new System.Drawing.Point(12, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(167, 31);
+            this.labelScore.TabIndex = 0;
+            this.labelScore.Text = "Score : 0";
+            this.labelScore.Click += new System.EventHandler(this.labelScore_Click);
+            // 
+            // labelFood
+            // 
+            this.labelFood.AutoSize = true;
+            this.labelFood.BackColor = System.Drawing.Color.Red;
+            this.labelFood.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFood.Location = new System.Drawing.Point(428, 245);
+            this.labelFood.Name = "labelFood";
+            this.labelFood.Size = new System.Drawing.Size(26, 27);
+            this.labelFood.TabIndex = 1;
+            this.labelFood.Text = "!";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(300, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(565, 340);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GAME OVER\r\n\r\nF - RESTART\r\nQ - QUIT\r\n";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelFood);
+            this.Controls.Add(this.labelScore);
+            this.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyDown);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelFood;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        int cols = 50, rows = 25, score = 0, dx = 0, dy = 0, front = 0, back = 0;
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        Piece[] snake = new Piece[1250];
+        List<int> available = new List<int>();
+        bool[,] visit;
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+           
+            
+
+
+
+            InitializeComponent();
+            Intial();
+            launchTimer();
+        }
+
+        private void launchTimer()
+        {
+            timer1.Interval = 100;
+            timer1.Tick += move;
+            timer1.Start();
+        }
+
+        private void Snake_KeyDown(object sender, KeyEventArgs e)
+        {
+            dx = dy = 0;
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    dx = 20;
+                    break;
+                case Keys.Left:
+                    dx = -20;
+                    break;
+                case Keys.Up:
+                    dy = -20;
+                    break;
+                case Keys.Down:
+                    dy = 20;
+                    break;
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                Environment.Exit(0);
+            }
+            if(e.KeyCode == Keys.F)
+            {
+                Application.Restart();
+            }
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void labelScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void move(object sender, EventArgs e)
+        {
+            int x = snake[front].Location.X, y = snake[front].Location.Y;
+                 if (dx == 0 && dy == 0) return;
+                 if(game_over(x + dx, y + dy))
+                  {
+                         timer1.Stop();
+                         MessageBox.Show("Game Over");
+                label1.Visible = true;
+                   return;
+                  }
+                 if(collisionFood(x + dx, y + dy))
+            {
+                        score += 1;
+                        labelScore.Text = "Score:" + score.ToString();
+
+                   if(hits((y + dy) / 20, (x + dx) / 20)) return;
+                  Piece head = new Piece(x + dx, y + dy);
+                      front = (front - 1 + 1000) % 1000;
+                         snake[front] = head;
+                          visit[head.Location.Y / 20, head.Location.X / 20] = true;
+                          Controls.Add(head);
+                RandomFood();
+            }
+            else
+            {
+                if (hits((y + dy) / 20, (x + dx) / 20)) return;
+                   visit[snake[back].Location.Y / 20, snake[back].Location.X / 20] = false;
+                  front = (front - 1 + 1000) % 1000;
+                  snake[front] = snake[back];
+                  snake[front].Location = new Point(x + dx, y + dy);
+                  back = (back - 1 + 1000) % 1000;
+                  visit[(y + dy) / 20, (x + dx) / 20] = true;
+
+            }
+        }
+
+        private void RandomFood()
+        {
+            available.Clear();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if(!visit[i, j])
+                    {
+                        available.Add(i * cols + j);
+                    }
+
+                }
+                int idx = rand.Next(available.Count) % available.Count;
+                    labelFood.Left = (available[idx] * 20) % Width;
+                 labelFood.Top = (available[idx] * 20) / Width * 20;
+
+            }
+        }
+
+        private bool hits(int x, int y)
+        {
+            if(visit[x, y])
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over");
+                return true;
+            }
+            return false;
+        }
+
+        private bool collisionFood(int x, int y)
+        {
+            return x == labelFood.Location.X && y == labelFood.Location.Y;
+        }
+
+        private bool game_over(int x, int y)
+        {
+            return x < 0 || y < 0 || x > 980 || y > 480;
+        }
+
+        private void Intial()
+        {
+            visit = new bool[rows, cols];
+            Piece head 
+                = new Piece((rand.Next() % cols) * 20, (rand.Next() % rows) * 20);
+             labelFood.Location 
+                = new Point((rand.Next() % cols) * 20, (rand.Next() % rows) * 20);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    visit[i, j] = false;
+                    available.Add(i * cols + j);
+
+                }
+                visit[head.Location.Y / 20, head.Location.X / 20] = true;
+                available.Remove(head.Location.Y / 20 * cols + head.Location.X / 20);
+                Controls.Add(head); snake[front] = head;
+            }
+        }
+
+           
+        
+    }
+}
